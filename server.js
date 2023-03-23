9db77eef22d774442c8ac9f509e042bab7390e23
Blain <97914052+ZinxValkyria@users.noreply.github.com>
2023-03-23 21:50:05 +0000

    fixed port number

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8081;
+const port = 8080;
 const mongoose = require('mongoose');
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
