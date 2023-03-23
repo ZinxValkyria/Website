2b7202c3da4e04121a21af39b641bae89e9f5c25
Blain <97914052+ZinxValkyria@users.noreply.github.com>
2023-03-23 21:45:54 +0000

    Configured MongoDB :)

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
-var express = require('express');
-var app = express();
-const port = 8080;
+const express = require('express');
+const app = express();
+const port = 8081;
+const mongoose = require('mongoose');
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// Replace the placeholder with your Atlas connection string
+const uri = "mongodb+srv://Blain:deadlyZ31@cluster0.dlimqkn.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+);
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
